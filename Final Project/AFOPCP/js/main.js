6a4d15c08e97928a27c019b097ba876342f8bf21
TheHoff55 <joseph-hoffman@uiowa.edu>
2025-05-04 21:25:26 -0500

    Updated PCP

--- Final Project/AFOPCP/js/main.js
+++ Final Project/AFOPCP/js/main.js
@@ -1,5 +1,5 @@
 
-var polygonJSONFile = "data/IowaZipAFOJoinedSimp.json";
+var polygonJSONFile = "data/AFOPointsGeoPostSimp.json";
 
 //change the center of your map in setView
 var centerLatitude = 42.17, centerLongitude = -93.45;
@@ -15,7 +15,8 @@ var key = "ZCTA";
 var dropdowntext = "Select Attribute:";
 
 //TODO: change the name of the attributes to include in the parallel coordinate plot
-var attNames = ["AFOZipTotals$.Cattle__", "AFOZipTotals$.Swine__", "AFOZipTotals$.Chicken__", "AFOZipTotals$.Prostate_Risk_Probability","AFOZipTotals$.NHL_Risk_Probability", "AFOZipTotals$.Colorectal_Risk_Probability","AFOZipTotals$.Lung_Risk_Probability"];
+var attNames = ["Pct_Cattle","Pct_Chicken","Pct_Swine","Prostate_Risk_Prob","NHL_Risk_Prob","Colorectal_Risk_Prob"
+  ,"Lung_Risk_Prob"];
 var attLegendFormat = ".0f"
 
 // This string is appended in front of the attribute name to make age groups descriptive
